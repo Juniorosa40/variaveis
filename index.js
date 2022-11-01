f9f6dce65666ccd769d00736a3ae91636d695c4f
Junior Rosa <acsrosajunior@gmail.com>
2022-11-01 15:12:48 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('João nasceu em 1984.');
+console.log('Em 2000 João conheceu Maria.');
+console.log('EM 2012 João casou-se com Maria.');
+console.log('Maria teve um filho com João em 2015.');
+console.log('O filho de João se chama Eduardo')
