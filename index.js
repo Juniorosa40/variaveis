2ea97ac242d55d20e443a15343d3160eb99752dc
Junior Rosa <acsrosajunior@gmail.com>
2022-11-01 15:26:28 -0300

    Variável declarada e no nosso codigo sempre que digitar-mos nome automaticamente o valor será utilizado.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log('João nasceu em 1984.');
-console.log('Em 2000 João conheceu Maria.');
-console.log('EM 2012 João casou-se com Maria.');
-console.log('Maria teve um filho com João em 2015.');
-console.log('O filho de João se chama Eduardo')
+// Vamos introduzir a variável let e declarar um valor.
+let nome = 'João'
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log('EM 2012', nome, 'casou-se com Maria.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo')
